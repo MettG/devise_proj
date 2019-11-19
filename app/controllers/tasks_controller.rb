508092b10c8ed2e0603fe175add7d0d356d19696
Tristan Johnson <tristan.t.johnson@gmail.com>
2019-11-18 23:37:17 -0700

    working commit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,49 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update]
+
   def index
+    @tasks = current_user.tasks
   end
 
   def show
   end
 
   def new
+    @task = Task.new
+  end
+
+  def create
+    @task = current_user.tasks.new(task_params)
+    if @task.save
+      redirect_to tasks_path
+    else
+      render :new
+    end
   end
 
   def edit
   end
+
+  def update
+    if @task.update(task_params)
+      redirect_to tasks_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+    def task_params
+      params.require(:task).permit(:start, :due, :body)
+    end
+
+    # don't just find by the Account model or you may potentially be able to view other users accounts
+    def set_task
+      @task = current_user.tasks.find(params[:id])
+    end
 end
